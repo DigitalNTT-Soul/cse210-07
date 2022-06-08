81868d652a60effaa49d56dad403cfe1f665449f
DigitalNTT-Soul <dylan.ruppell@gmail.com>
2022-06-08 15:06:30 -0400

    fixed RNG color of debris

--- greed/game/casting/debris.py
+++ greed/game/casting/debris.py
@@ -4,7 +4,7 @@ from game.shared.color import Color
 from random import randint
 
 class Debris(Actor):
-    def __init__(self, value: int = 0, text: str = "", position: Point = Point(0, 0), font_size: int = 15, color: Color = Color(randint(0,255), randint(0,255), randint(0,255)), velocity: Point = Point(0, 0)):
+    def __init__(self, value: int, text: str, position: Point, font_size: int = 15, velocity: Point = Point(0, 0)):
 
         super().__init__()
         self._value = value
@@ -12,7 +12,7 @@ class Debris(Actor):
         self._position = position
         self._velocity = velocity
         self._font_size = font_size
-        self._color = color
+        self._color = Color(randint(0,255),randint(0,255),randint(0,255))
     
     def get_value(self):
         return self._value
